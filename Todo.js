f74c9bc0a92dfa2fc6dd88e41168e82ccbc3063e
hajimev7 <57899416+hajimev7@users.noreply.github.com>
2022-05-23 21:11:58 +0900

    編集機能変更

--- Todo.js
+++ Todo.js
@@ -1,6 +1,9 @@
 const form = document.getElementById("form");
+const addbutton = document.getElementById("addbutton")
 const input = document.getElementById("input");
+const input2 = document.createElement('input');
 const ul = document.getElementById("ul")
+const span = document.getElementById('span');
 
 const todos=JSON.parse(localStorage.getItem("todos"));
 
@@ -9,6 +12,10 @@ if (todos) {
     add(todo);
   })
 }
+//追加ボタンの機能追加x
+addbutton.addEventListener('click',()=>{
+  add()
+})
 
 form.addEventListener("submit",function(event){
   event.preventDefault();
@@ -27,7 +34,7 @@ function add(todo){
   const deleteButton = document.createElement('button');
   const statusButton = document.createElement('button');
   const editButton = document.createElement('button');
-
+  const edit2Button = document.createElement('button');       
 
   li.classList.add("list-group-item")
   span.textContent = todoText;
@@ -38,6 +45,7 @@ function add(todo){
 
     deleteButton.addEventListener('click',()=>{ 
       li.remove()
+      saveData();
     })
 
     statusButton.innerText = '未完了';
@@ -50,28 +58,31 @@ function add(todo){
         statusButton.innerText = '未完了';
       }
   });
-//編集ボタンの追加
+
+  //編集ボタンの追加
     editButton.innerText = '編集';
     li.appendChild(editButton);
-//編集機能の追加
-    editButton.addEventListener('click',()=>{
-      console.log(span)
+  //編集機能の追加
+    editButton.addEventListener('click',()=>{ 
       
-      input.value = span.innerHTML
-    
-      li.remove()
+      span.remove()
+      console.log(span)
+
+      input2.setAttribute("id", "input2");
+      edit2Button.innerText = '編集完了';
+      li.appendChild(edit2Button);   
+      input2.value = span.innerHTML
+      li.prepend(input2);   
+      saveData();
+      
     })
 
-  
-  
-  li.addEventListener("contextmenu",function(event){
-   event.preventDefault();
-   li.remove();
-  })
-
-  li.addEventListener("click",function(){
-    li.classList.toggle("text-decoration-line-through")
-  })
+    edit2Button.addEventListener('click',()=>{
+      span.textContent = input2.value;
+      li.prepend(span)
+      input2.remove()  
+      edit2Button.remove() 
+    })
 
   ul.appendChild(li)
   input.value="";
@@ -80,7 +91,8 @@ function add(todo){
 }
 
 function saveData(){
-  const lists = document.querySelectorAll("li");
+  //spanタグの取得に変更
+  const lists = document.querySelectorAll("span");
   let todos=[]
   lists.forEach(list=>{
     todos.push(list.innerText)
